923f2a0c81110965d22ff85e8d7c22ca318c8909
Daniel Luque Quintana <dranko91@gmail.com>
2020-04-14 13:35:52 +0200

    Add missing celery file

---
+++ project/celery.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from datetime import timedelta
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+app = Celery('nluproxy', broker=os.environ.get('BROKER_URL'))
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
